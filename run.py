cfe06a318123634d043a71c95d1ac94aa3a5d9ef
Hampus Strate <stratehampus02@gmail.com>
2022-11-09 11:58:06 +0000

    Add structure to the quiz and questions for the quiz

--- run.py
+++ run.py
@@ -1,3 +1,34 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("Welcome to the software programming quiz")
+
+playing = input("Do you want to play the quiz? ")
+name = input()
+
+if playing.lower() != "yes":
+    quit()
+
+print("Let's start the game!")
+score = 0
+
+answer = input("What does HTML stand for? ")
+if answer == "hypertext markup language":
+    print("Correct!!")
+    score += 1
+else:
+    print("Wrong answer!")
+
+answer = input("What does CSS stand for? ")
+if answer == "cascading style sheets":
+    print("Correct!!")
+    score += 1
+else:
+    print("Wrong answer!")
+
+answer = input("What does JS stand for? ")
+if answer == "javascript":
+    print("Correct!!")
+    score += 1
+else:
+    print("Wrong answer!")
+
+print("Your score is " + str(score))
+print("You got " + str(score / 4) + "%.")
