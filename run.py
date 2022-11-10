30023ae678592642c48e36b1fa6abb9015df8f8a
Hampus Strate <stratehampus02@gmail.com>
2022-11-10 09:56:54 +0000

    Add questions and a format for the questions to be put in

--- run.py
+++ run.py
@@ -1,34 +1,91 @@
-print("Welcome to the software programming quiz")
+import pathlib
+import random
+from string import ascii_lowercase
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
 
-playing = input("Do you want to play the quiz? ")
-name = input()
+NUM_QUESTIONS_PER_QUIZ = 10
+QUESTIONS_PATH = pathlib.Path(__file__).parent / "questions.toml"
 
-if playing.lower() != "yes":
-    quit()
+def run_quiz():
+    questions = prepare_questions(
+        QUESTIONS_PATH, num_questions=NUM_QUESTIONS_PER_QUIZ
+    )
 
-print("Let's start the game!")
-score = 0
+    num_correct = 0
+    for num, question in enumerate(questions, start=1):
+        print(f"\nQuestion {num}:")
+        num_correct += ask_question(question)
 
-answer = input("What does HTML stand for? ")
-if answer == "hypertext markup language":
-    print("Correct!!")
-    score += 1
-else:
-    print("Wrong answer!")
+    print(f"\nYou got {num_correct} correct out of {num} questions")
 
-answer = input("What does CSS stand for? ")
-if answer == "cascading style sheets":
-    print("Correct!!")
-    score += 1
-else:
-    print("Wrong answer!")
+def prepare_questions(path, num_questions):
+    topic_info = tomllib.loads(path.read_text())
+    topics = {
+        topic["label"]: topic["questions"] for topic in topic_info.values()
+    }
+    topic_label = get_answers(
+        question="What difficulty do you choose?",
+        alternatives=sorted(topics),
+    )[0]
 
-answer = input("What does JS stand for? ")
-if answer == "javascript":
-    print("Correct!!")
-    score += 1
-else:
-    print("Wrong answer!")
+    questions = topics[topic_label]
+    num_questions = min(num_questions, len(questions))
+    return random.sample(questions, k=num_questions)
 
-print("Your score is " + str(score))
-print("You got " + str(score / 4) + "%.")
+def ask_question(question):
+    correct_answers = question["answers"]
+    alternatives = question["answers"] + question["alternatives"]
+    ordered_alternatives = random.sample(alternatives, k=len(alternatives))
+
+    answers = get_answers(
+        question=question["question"],
+        alternatives=ordered_alternatives,
+        num_choices=len(correct_answers),
+        hint=question.get("hint"),
+    )
+    if correct := (set(answers) == set(correct_answers)):
+        print("Correct!")
+    else:
+        is_or_are = " is" if len(correct_answers) == 1 else "s are"
+        print("\n- ".join([f"No, the answer{is_or_are}:"] + correct_answers))
+
+    if "explanation" in question:
+        print(f"\nEXPLANATION:\n{question['explanation']}")
+
+    return 1 if correct else 0
+
+def get_answers(question, alternatives, num_choices=1, hint=None):
+    print(f"{question}?")
+    labeled_alternatives = dict(zip(ascii_lowercase, alternatives))
+
+    for label, alternative in labeled_alternatives.items():
+        print(f"  {label}) {alternative}")
+
+    while True:
+        plural_s = "" if num_choices == 1 else f"s (choose {num_choices})"
+        answer = input(f"\nChoice{plural_s}? ")
+        answers = set(answer.replace(",", " ").split())
+
+        # Handle invalid answers
+        if len(answers) != num_choices:
+            plural_s = "" if num_choices == 1 else "s, separated by comma"
+            print(f"Please answer {num_choices} alternative{plural_s}")
+            continue
+
+        if any(
+            (invalid := answer) not in labeled_alternatives
+            for answer in answers
+        ):
+            print(
+                f"{invalid!r} is not a valid choice. "
+                f"Please use {', '.join(labeled_alternatives)}"
+            )
+            continue
+
+        return [labeled_alternatives[answer] for answer in answers]
+        
+if __name__ == "__main__":
+    run_quiz()
