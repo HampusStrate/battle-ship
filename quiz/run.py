5f494537164994e374d0d4f411aa4b9423c3f65e
Hampus Strate <stratehampus02@gmail.com>
2022-11-23 11:58:59 +0000

    I hade a very good ide it dident work f or some reason the toml file dident want to coonect so i dont know what i did wrong

--- quiz/run.py
+++ quiz/run.py
@@ -1,91 +1,92 @@
-import pathlib
+# quiz.py
+
 import random
 from string import ascii_lowercase
-try:
-    import tomllib
-except ModuleNotFoundError:
-    import tomli as tomllib
 
-NUM_QUESTIONS_PER_QUIZ = 8
-QUESTIONS_PATH = pathlib.Path(__file__).parent / "questions.toml"
+NUM_QUESTIONS_PER_QUIZ = 5
+QUESTIONS = {
+    "Which built-in function can get information from the user": [
+        "input", "get", "print", "write",
+    ],
+    "What does 'HTML' stand for?": [
+        "hypertext markup language", "hypertitle makeup language", "hyper text markup language",
+    ],
+    "What does 'CSS' stand for?": [
+        "Cascading style sheets",
+        "Cascading sheets style",
+        "Control sample styles",
+        "Contact style sheets",
+    ],
+    "What's the name of Python's sorting algorithm": [
+        "Timsort", "Quicksort", "Merge sort", "Bubble sort",
+    ],
+    "What does dict.get(key) return if key isn't found in dict": [
+        "None", "key", "True", "False",
+    ],
+    "How do you iterate over both indices and elements in an iterable": [
+        "enumerate(iterable)",
+        "enumerate(iterable, start=1)",
+        "range(iterable)",
+        "range(iterable, start=1)",
+    ],
+    "What's the official name of the := operator": [
+        "Assignment expression",
+        "Named expression",
+        "Walrus operator",
+        "Colon equals operator",
+    ],
+    "What's one effect of calling random.seed(42)": [
+        "The random numbers are reproducible.",
+        "The random numbers are more random.",
+        "The computer clock is reset.",
+        "The first random number is always 42.",
+    ],
+    "When does __name__ == '__main__' equal True in a Python file": [
+        "When the file is run as a script",
+        "When the file is imported as a module",
+        "When the file has a valid name",
+        "When the file only has one function",
+    ]
+}
 
 def run_quiz():
     questions = prepare_questions(
-        QUESTIONS_PATH, num_questions=NUM_QUESTIONS_PER_QUIZ
+        QUESTIONS, num_questions=NUM_QUESTIONS_PER_QUIZ
     )
 
     num_correct = 0
-    for num, question in enumerate(questions, start=1):
+    for num, (question, alternatives) in enumerate(questions, start=1):
         print(f"\nQuestion {num}:")
-        num_correct += ask_question(question)
+        num_correct += ask_question(question, alternatives)
 
     print(f"\nYou got {num_correct} correct out of {num} questions")
 
-def prepare_questions(path, num_questions):
-    topic_info = tomllib.loads(path.read_text())
-    topics = {
-        topic["label"]: topic["questions"] for topic in topic_info.values()
-    }
-    topic_label = get_answers(
-        question="What difficulty do you choose?",
-        alternatives=sorted(topics),
-    )[0]
-
-    questions = topics[topic_label]
+def prepare_questions(questions, num_questions):
     num_questions = min(num_questions, len(questions))
-    return random.sample(questions, k=num_questions)
+    return random.sample(list(questions.items()), k=num_questions)
 
-def ask_question(question):
-    correct_answers = question["answers"]
-    alternatives = question["answers"] + question["alternatives"]
+def ask_question(question, alternatives):
+    correct_answer = alternatives[0]
     ordered_alternatives = random.sample(alternatives, k=len(alternatives))
 
-    answers = get_answers(
-        question=question["question"],
-        alternatives=ordered_alternatives,
-        num_choices=len(correct_answers),
-        hint=question.get("hint"),
-    )
-    if correct := (set(answers) == set(correct_answers)):
-        print("Correct!")
+    answer = get_answer(question, ordered_alternatives)
+    if answer == correct_answer:
+        print("Good Job!")
+        return 1
     else:
-        is_or_are = " is" if len(correct_answers) == 1 else "s are"
-        print("\n- ".join([f"No, the answer{is_or_are}:"] + correct_answers))
+        print(f"The answer is {correct_answer!r}, not {answer!r}")
+        return 0
 
-    if "explanation" in question:
-        print(f"\nEXPLANATION:\n{question['explanation']}")
-
-    return 1 if correct else 0
-
-def get_answers(question, alternatives, num_choices=1, hint=None):
+def get_answer(question, alternatives):
     print(f"{question}?")
     labeled_alternatives = dict(zip(ascii_lowercase, alternatives))
-
     for label, alternative in labeled_alternatives.items():
         print(f"  {label}) {alternative}")
 
-    while True:
-        plural_s = "" if num_choices == 1 else f"s (choose {num_choices})"
-        answer = input(f"\nChoice{plural_s}? ")
-        answers = set(answer.replace(",", " ").split())
+    while (answer_label := input("\nChoice? ")) not in labeled_alternatives:
+        print(f"Please answer one of {', '.join(labeled_alternatives)}")
 
-        # Handle invalid answers
-        if len(answers) != num_choices:
-            plural_s = "" if num_choices == 1 else "s, separated by comma"
-            print(f"Please answer {num_choices} alternative{plural_s}")
-            continue
+    return labeled_alternatives[answer_label]
 
-        if any(
-            (invalid := answer) not in labeled_alternatives
-            for answer in answers
-        ):
-            print(
-                f"{invalid!r} is not a valid choice. "
-                f"Please use {', '.join(labeled_alternatives)}"
-            )
-            continue
-
-        return [labeled_alternatives[answer] for answer in answers]
-        
 if __name__ == "__main__":
     run_quiz()
